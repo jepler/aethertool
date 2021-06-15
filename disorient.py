e2635982aab85ed3d0efd9825b7108076fb0bec4
Jeff Epler <jepler@gmail.com>
2021-06-15 09:38:18 -0500

    format with black

--- disorient.py
+++ disorient.py
@@ -4,26 +4,28 @@
 #   This program is free software: you can redistribute it and/or modify it
 #   under the terms of the GNU General Public License version 3 as published by
 #   the Free Software Foundation.
-#   
+#
 #   This program is distributed in the hope that it will be useful, but WITHOUT
 #   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 #   more details.
-#   
+#
 #   You should have received a copy of the GNU General Public License along
 #   with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# This program is a new implementation of the method described at 
+# This program is a new implementation of the method described at
 #  http://sylvana.net/jpegcrop/exif_orientation.html
 # and in the "jpegexiforient.c" source code.
 
-EXIF_MAGIC = '\xff\xd8\xff\xe1'      #  JPEG SOI + Exif APP1
-EXIF_MAGIC_2 = 'Exif\0\0'
+EXIF_MAGIC = "\xff\xd8\xff\xe1"  #  JPEG SOI + Exif APP1
+EXIF_MAGIC_2 = "Exif\0\0"
 
 import struct
 
+
 def read_2(f):
     return ord(f.read(1)) * 256 + ord(f.read(1))
+
 
 def exif_orientation(f, set=None):
     if set:
@@ -32,77 +34,95 @@ def exif_orientation(f, set=None):
         f = open(f, "rb")
 
     buf = f.read(4)
-    #print `buf`
+    # print `buf`
     if buf != EXIF_MAGIC:
         return
 
     length = read_2(f)
-    #print `length`
-    if length <= 20: return
-    
+    # print `length`
+    if length <= 20:
+        return
+
     buf = f.read(6)
-    #print `buf`
+    # print `buf`
     if buf != EXIF_MAGIC_2:
         return
 
     buf = f.read(length - 8)
-    #print `buf[:2]`
-    if buf[:2] == 'II': is_motorola, order = 0, "<"
-    elif buf[:2] == 'MM': is_motorola, order = 1, ">"
-    else: return
-
-    decode_4 = lambda i: struct.unpack(order + "I", buf[i:i+4])[0]
-    decode_2 = lambda i: struct.unpack(order + "H", buf[i:i+2])[0]
-    #print "here", repr(buf[2:4])
-    if is_motorola:
-        if buf[2:4] != '\0\x2a': return
+    # print `buf[:2]`
+    if buf[:2] == "II":
+        is_motorola, order = 0, "<"
+    elif buf[:2] == "MM":
+        is_motorola, order = 1, ">"
     else:
-        if buf[2:4] != '\x2a\0': return
+        return
 
+    decode_4 = lambda i: struct.unpack(order + "I", buf[i : i + 4])[0]
+    decode_2 = lambda i: struct.unpack(order + "H", buf[i : i + 2])[0]
+    # print "here", repr(buf[2:4])
+    if is_motorola:
+        if buf[2:4] != "\0\x2a":
+            return
+    else:
+        if buf[2:4] != "\x2a\0":
+            return
 
     offset = decode_4(4)
-    #print offset
-    if offset > length - 2: return
+    # print offset
+    if offset > length - 2:
+        return
 
     number_of_tags = decode_2(offset)
     offset += 2
-    #print "here", offset, length
+    # print "here", offset, length
 
     while 1:
-        if offset > length - 12: return
+        if offset > length - 12:
+            return
         tagnum = decode_2(offset)
-        #print "loop", offset, length, tagnum
-        if tagnum == 0x112: break
+        # print "loop", offset, length, tagnum
+        if tagnum == 0x112:
+            break
         number_of_tags -= 1
-        if not number_of_tags: return 0
+        if not number_of_tags:
+            return 0
         offset += 12
 
     if not set:
-        #print repr(buf[offset:offset+12])
-        return decode_2(offset+8)
+        # print repr(buf[offset:offset+12])
+        return decode_2(offset + 8)
     else:
         if is_motorola:
-            new_tag = '\0\3' '\0\0\0\1' '\0' + chr(set) + '\0\0'
+            new_tag = "\0\3" "\0\0\0\1" "\0" + chr(set) + "\0\0"
         else:
-            new_tag = '\3\0' '\1\0\0\0' + chr(set) + '\0' '\0\0'
+            new_tag = "\3\0" "\1\0\0\0" + chr(set) + "\0" "\0\0"
     f.seek(4 + 2 + 6 + 2 + offset)
     f.write(new_tag)
     f.seek(0, 2)
-    #print "replaced"
+    # print "replaced"
 
-def clear_exif_orientation(x): return exif_orientation(x, 1)
+
+def clear_exif_orientation(x):
+    return exif_orientation(x, 1)
+
 
 descr = {
-    1: "Normal", 2: "Mirrored left-to-right", 3: "Rotated 180 degrees",
-    4: "Mirrored top-to-bottom", 5: "Mirrored along top-left diagonal",
-    6: "Rotated 90 degrees", 7: "Mirrored along top-right diagonal",
-    8: "Rotated 270 degrees"
+    1: "Normal",
+    2: "Mirrored left-to-right",
+    3: "Rotated 180 degrees",
+    4: "Mirrored top-to-bottom",
+    5: "Mirrored along top-left diagonal",
+    6: "Rotated 90 degrees",
+    7: "Mirrored along top-right diagonal",
+    8: "Rotated 270 degrees",
 }
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     if sys.argv[1] == "-c":
-        clear_exif_orientation(sys.argv[2]) 
+        clear_exif_orientation(sys.argv[2])
         print("Orientation cleared")
-    else: print(descr.get(exif_orientation(sys.argv[1]), "Invalid orientation"))
+    else:
+        print(descr.get(exif_orientation(sys.argv[1]), "Invalid orientation"))
 # vim:sw=4:sts=4:et
--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@
 #   This program is free software: you can redistribute it and/or modify it
 #   under the terms of the GNU General Public License version 3 as published by
 #   the Free Software Foundation.
-#   
+#
 #   This program is distributed in the hope that it will be useful, but WITHOUT
 #   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 #   more details.
-#   
+#
 #   You should have received a copy of the GNU General Public License along
 #   with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -18,6 +18,8 @@
 from distutils.core import setup
 
 import os
+
+
 def require_module(m):
     try:
         __import__(m)
@@ -26,8 +28,9 @@ def require_module(m):
         print("The software will probably not work without it.")
         return False
 
+
 def require_program(s):
-    for el in os.environ['PATH'].split(os.pathsep):
+    for el in os.environ["PATH"].split(os.pathsep):
         j = os.path.join(el, s)
         if os.access(j, os.F_OK | os.X_OK):
             return True
@@ -35,16 +38,20 @@ def require_program(s):
     print("The software will probably not work without it.")
     return False
 
+
 for m in ("Image",):
     require_module(m)
 
 for p in ("pngcrush", "jpegtran"):
     require_program(p)
 
-setup(name="aethertool", version="0.7",
-    author="Jeff Epler", author_email = "jepler@unpythonic.net",
-    py_modules=['disorient'],
-    scripts=['aether'],
+setup(
+    name="aethertool",
+    version="0.7",
+    author="Jeff Epler",
+    author_email="jepler@unpythonic.net",
+    py_modules=["disorient"],
+    scripts=["aether"],
     url="http://emergent.unpy.net/software/aethertool/",
     license="GPL",
 )
